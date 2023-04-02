73139193c2ef3457ba79838089e9421cd2108a37
Jon Stricoff <stricoff92@gmail.com>
2023-04-02 13:45:10 -0400

    wip

--- csauth/scripts/unix_to_tsv.py
+++ csauth/scripts/unix_to_tsv.py
@@ -2,8 +2,11 @@
 from logging import Logger
 
 from common.tmp_file_wrapper import OutputFileWrapper
+from common.command_runner import CommandRunner
 
 def main(logger: Logger):
     logger.info("unix_to_tsv::main()")
     output = OutputFileWrapper()
 
+    cmd = CommandRunner('ls /etc')
+    print(cmd.read_result())
