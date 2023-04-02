847f5f39e1643a937b6751e6adb0d84f05882bb9
Jon Stricoff <stricoff92@gmail.com>
2023-04-02 14:04:49 -0400

    wip

--- csauth/scripts/unix_to_tsv.py
+++ csauth/scripts/unix_to_tsv.py
@@ -4,9 +4,42 @@ from logging import Logger
 from common.tmp_file_wrapper import OutputFileWrapper
 from common.command_runner import CommandRunner
 
+# YP files
+PASSWD_FILE = '/var/yp/passwd'
+SHADOW_FILE = '/var/yp/shadow'
+GROUP_FILE = '/etc/group'
+
+# Regular UNIX files
+# PASSWD_FILE = '/etc/passwd'
+# SHADOW_FILE = '/etc/shadow'
+# GROUP_FILE = '/etc/group'
+
+def _build_output(
+    output: OutputFileWrapper,
+    cat_passwd_cmd,
+    cat_shadow_cmd,
+    cat_group_cmd
+):
+    pass
+
 def main(logger: Logger):
     logger.info("unix_to_tsv::main()")
     output = OutputFileWrapper()
+    cat_passwd_cmd = CommandRunner(f'cat {PASSWD_FILE}')
+    cat_shadow_cmd = CommandRunner(f'cat {SHADOW_FILE}')
+    cat_group_cmd = CommandRunner(f'cat {GROUP_FILE}')
 
-    cmd = CommandRunner('ls /etc')
-    print(cmd.read_result())
+    try:
+        _build_output(
+            output,
+            cat_passwd_cmd,
+            cat_shadow_cmd,
+            cat_group_cmd,
+        )
+    except Exception:
+        raise
+    finally:
+        logger.info("deleting cat output files...")
+        cat_passwd_cmd.delete_results()
+        cat_shadow_cmd.delete_results()
+        cat_group_cmd.delete_results()
